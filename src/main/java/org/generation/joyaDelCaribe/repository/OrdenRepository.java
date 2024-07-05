a8e3502735f15be7023440bb09052f18d352ce93
esdrasmdz <esdrasm.diazz@gmail.com>
2024-07-05 13:07:57 -0600

    avance ordenrepository

--- src/main/java/org/generation/joyaDelCaribe/repository/OrdenRepository.java
+++ src/main/java/org/generation/joyaDelCaribe/repository/OrdenRepository.java
@@ -1,5 +1,14 @@
 package src.main.java.org.generation.joyaDelCaribe.repository;
+import java.util.Optional;
 
-public interface OrdenRepository {
+import org.generation.ecommerce.model.Categoria;
+import org.generation.ecommerce.model.Orden;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdenRepository extends JpaRepository<Orden, Integer>{
+	Optional<Orden> findByNombre(String nombre);
+	
 }
